b09ec7474b6c7ab014feff3efcca035414543c89
YuliiaShatkovska <113515907+YuliiaShatkovska@users.noreply.github.com>
2024-03-23 11:52:53 +0000

    Deploying to gh-pages from @ YuliiaShatkovska/your_campers@841a86594cb748fcc7e03765e403afb7d3bad281 🚀

---
+++ static/js/339.4f5e069b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyour_campers=self.webpackChunkyour_campers||[]).push([[339],{2339:function(e,r,u){u.r(r);var c=u(4169),n=u(184);r.default=function(){return(0,n.jsx)(c.W,{children:(0,n.jsx)("h1",{children:"favorite"})})}}}]);
+//# sourceMappingURL=339.4f5e069b.chunk.js.map
